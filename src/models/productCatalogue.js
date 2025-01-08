42da9c57fa83923ce16f04f1ccc46a6284e914fa
JunaidSFT <78009418+JunaidSFT@users.noreply.github.com>
2025-01-09 00:03:24 +0500

    added product catalogue in models

---
+++ src/models/productCatalogue.js
@@ -0,0 +1,8 @@
+const products = [
+    {id: "001", label: "Silver Label", price: 100, discount: {quantity: 3, price: 200 }},
+    {id: "002", label: "Gold Label", price: 80, discount: {quantity: 2, price: 120 }},
+    {id: "003", label: "Platinum Label", price: 50, discount: null},
+    {id: "004", label: "Diamond Label", price: 30, discount: null}
+];
+
+module.exports = products;
