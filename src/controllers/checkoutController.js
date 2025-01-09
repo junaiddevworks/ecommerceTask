74f9f8e0217d489827b80f36e4dc5b5efafa9f3b
JunaidSFT <78009418+JunaidSFT@users.noreply.github.com>
2025-01-09 12:15:02 +0500

    added Joi for input validation

--- src/controllers/checkoutController.js
+++ src/controllers/checkoutController.js
@@ -1,15 +1,23 @@
+const Joi = require('joi');
 const checkoutService = require('../services/checkoutService');
 
+
+const checkoutSchema = Joi.array().items(Joi.string().length(3).required()).required();
+
 exports.checkout = (req, res, next) => {
-    try {
-      const items = req.body;
-      if(!Array.isArray(items) || items.some((item) => typeof item !=='string')) {
-        return res.status(400).json({message: 'Invalid input!'})
-      }
-  
-      const totalPrice = checkoutService.calculateTotal(items);
-      return res.status(200).json({ price: totalPrice });
-    } catch (error) {
-      next(error)
+  try {
+    
+    const { error } = checkoutSchema.validate(req.body);
+
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
     }
-  };
+
+    //if passes validation
+    const items = req.body;
+    const totalPrice = checkoutService.calculateTotal(items);
+    return res.status(200).json({ price: totalPrice });
+  } catch (err) {
+    next(err);
+  }
+};
